f6fc37f01c2212fdd201dc36ae5f1fa6f4fdc761
zikwall <andrey.kapitonov.96@gmail.com>
2019-12-01 16:08:29 +0300

    :pencil2: Add viewport meta

--- src/views/layouts/embed.php
+++ src/views/layouts/embed.php
@@ -12,6 +12,7 @@ use yii\helpers\Html;
     <html lang="en">
     <head>
         <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <?= Html::csrfMetaTags() ?>
         <?php $this->head() ?>
     </head>
