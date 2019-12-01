2bef21aab598cdbaf27b16825817992120d77734
zikwall <andrey.kapitonov.96@gmail.com>
2019-12-01 15:37:24 +0300

    :bug: Fix after

--- src/views/embed/give.php
+++ src/views/embed/give.php
@@ -1,12 +1,15 @@
 <div id="jw"></div>
 
-<script type="text/JavaScript">
+<?php
 
+$JW = <<<JS
     jwplayer("jw").setup({
         file: 'https://83tpjqlujs1.a.trbcdn.net/livemaster/tftrm1v2h9_stream1/playlist.m3u8',
         image: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg',
         autostart: true,
         mute: true
     });
+JS;
 
-</script>
+$this->registerJs($JW, ['position' => \yii\web\View::POS_END]);
+
