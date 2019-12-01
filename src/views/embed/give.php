609faa9a000e54fb5b619bad697fcf4208d08c1d
zikwall <andrey.kapitonov.96@gmail.com>
2019-12-01 15:34:50 +0300

    :pencil2: Add Player

--- src/views/embed/give.php
+++ src/views/embed/give.php
@@ -1,1 +1,12 @@
-Hi
+<div id="jw"></div>
+
+<script type="text/JavaScript">
+
+    jwplayer("jw").setup({
+        file: 'https://83tpjqlujs1.a.trbcdn.net/livemaster/tftrm1v2h9_stream1/playlist.m3u8',
+        image: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg',
+        autostart: true,
+        mute: true
+    });
+
+</script>
