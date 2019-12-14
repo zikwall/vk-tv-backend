87d78dd22a022ea5f6de58cb380b1977943ae894
zikwall <andrey.kapitonov.96@gmail.com>
2019-12-14 17:22:33 +0300

    Add ads

--- src/views/embed/give.php
+++ src/views/embed/give.php
@@ -11,6 +11,10 @@ $JW = <<<JS
         image: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg',
         autostart: true,
         mute: true,
+        advertising: {
+            client: 'vast',
+            tag: 'https://an.yandex.ru/meta/347075?imp-id=2&charset=UTF-8&target-ref=http%3A%2F%2Flimehd.ru&page-ref=http%3A%2F%2Flimehd.ru&rnd=1323233243'
+        }
     });
     
     player.on('pause', (event) => {
