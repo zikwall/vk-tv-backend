1a3961513468da53b5e1bdd84e462870b0263913
zikwall <andrey.kapitonov.96@gmail.com>
2020-01-01 18:47:08 +0300

    Temporary remove ADS

--- src/views/embed/give_openplayer.php
+++ src/views/embed/give_openplayer.php
@@ -10,12 +10,12 @@ $JW = <<<JS
     };
     
     let adUrls = [
-        'https://an.yandex.ru/meta/347075?imp-id=2&charset=UTF-8&target-ref=http%3A%2F%2Flimehd.ru&page-ref=http%3A%2F%2Flimehd.ru&rnd={random}',
-        'https://out.pladform.ru/getVast?pl=116720&type=preroll&license=1&thematic=1762&age=5&duration=0&target=mobile&adformat=3&dl=https://limehd.tv',
-        'https://px.adhigh.net/p/direct_vast?pid=172&tid=instream_spbtv',
-        'https://out.pladform.ru/getVast?pl=116722&type=preroll&license=1&thematic=1762&age=4&duration=0&target=mobile&adformat=3&dl=https://limehd.tv',
-        'https://an.yandex.ru/meta/290606?imp-id=2&charset=UTF-8&target-ref=http%3A%2F%2Flimehd.ru&page-ref=http%3A%2F%2Flimehd.ru&rnd={random}',
-        'https://data.videonow.ru/?profile_id=2458118&format=vast&container=preroll'
+        //'https://an.yandex.ru/meta/347075?imp-id=2&charset=UTF-8&target-ref=http%3A%2F%2Flimehd.ru&page-ref=http%3A%2F%2Flimehd.ru&rnd={random}',
+        //'https://out.pladform.ru/getVast?pl=116720&type=preroll&license=1&thematic=1762&age=5&duration=0&target=mobile&adformat=3&dl=https://limehd.tv',
+        //'https://px.adhigh.net/p/direct_vast?pid=172&tid=instream_spbtv',
+        //'https://out.pladform.ru/getVast?pl=116722&type=preroll&license=1&thematic=1762&age=4&duration=0&target=mobile&adformat=3&dl=https://limehd.tv',
+        //'https://an.yandex.ru/meta/290606?imp-id=2&charset=UTF-8&target-ref=http%3A%2F%2Flimehd.ru&page-ref=http%3A%2F%2Flimehd.ru&rnd={random}',
+        //'https://data.videonow.ru/?profile_id=2458118&format=vast&container=preroll'
     ];
     
     const getAdArray = () => {
