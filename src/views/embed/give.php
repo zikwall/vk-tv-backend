ca6b97b825476cf11bd048756977a707b560bead
zikwall <andrey.kapitonov.96@gmail.com>
2020-01-02 01:13:45 +0300

    AD Default by Google Single VPAID 2.0 Linear

--- src/views/embed/give.php
+++ src/views/embed/give.php
@@ -39,7 +39,7 @@ $JW = <<<JS
         mute: false,
         advertising: {
             client: 'vast',
-            tag: adUrl
+            tag: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinearvpaid2js&correlator='
         }
     });
     
