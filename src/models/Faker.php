7335319eb25877f4c62f7a299887872ab6a1b9a7
zikwall <andrey.kapitonov.96@gmail.com>
2020-01-05 16:06:53 +0300

    ept :bug:

--- src/models/Faker.php
+++ src/models/Faker.php
@@ -6,7 +6,7 @@ namespace vktv\models;
 
 class Faker
 {
-    public static function fullDayEpg($date)
+    public static function fullDayEpg(string $date)
     {
         $epg = [];
 
@@ -15,7 +15,7 @@ class Faker
 
             $epg[] = [
                 'time' => $hour . ':00 - ' . $hour . ':59',
-                'name' => 'Example TV Program ' + $date,
+                'name' => 'Example TV Program ' . $date,
                 'description' => 'Non description'
             ];
         };
