6d5a24d75978ffe42d13d84413363103902db057
zikwall <andrey.kapitonov.96@gmail.com>
2020-01-26 17:04:37 +0300

    :bug: Content class forgotten

---
+++ src/constants/Content.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace zikwall\vktv\constants;
+
+
+class Content
+{
+    const VISIBILITY_PUBLIC = 10;
+    const VISIBILITY_PRIVATE = 20;
+    const VISIBILITY_OWNER = 30;
+    const VISIBILITY_PREMIUM = 40;
+
+    const TYPE_CHANNEL = 10;
+    const TYPE_FILM = 20;
+    
+    const CONTENT_PINNED = 1;
+    const CONTENT_NOTPINNED = 0;
+    const CONTENT_ARCHIVED = 1;
+    const CONTENT_NOTARCHIVED = 0;
+}
