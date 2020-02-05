3894e35f9b9361acdd364313a9a0dc19540302be
AndreyKapitonov <dj-on-ik@mail.ru>
2020-02-05 15:45:20 +0300

    Random playlist get

--- src/controllers/ParseController.php
+++ src/controllers/ParseController.php
@@ -7,6 +7,22 @@ use zikwall\m3ucontentparser\M3UItem;
 
 class ParseController extends BaseController
 {
+    const PLAYLISTS = [
+        'https://iptvm3u.ru/iptv-hd/',
+        'https://iptvm3u.ru/one/',
+        'https://raw.githubusercontent.com/vasiliy78L/myIPTV/master/iptv.m3u',
+        'http://4pda.ru/pages/go/?u=http%3A%2F%2Ftopplay.do.am%2FFreeBestTV.m3u&e=84875135',
+        'https://webhalpme.ru/if.m3u'
+    ];
+
+    public function actionRandom()
+    {
+        return $this->response([
+            'code' => 200,
+            'response' => self::PLAYLISTS[array_rand(self::PLAYLISTS)]
+        ]);
+    }
+
     public function actionItems(string $url)
     {
         $items = [];
