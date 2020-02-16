1cb3eb5289a97e7a5c6049437db1c0f7b605a747
zikwall <andrey.kapitonov.96@gmail.com>
2020-02-16 12:24:04 +0300

    Add custom AD to openplayer

--- src/views/embed/give_openplayer.php
+++ src/views/embed/give_openplayer.php
@@ -3,37 +3,18 @@
 </video>
 
 <?php
-$JW = <<<JS
-    const random = (min, max) => {
-        let rand = min - 0.5 + Math.random() * (max - min + 1);
-        return Math.round(rand);
-    };
-    
-    let adUrls = [
-        //'https://an.yandex.ru/meta/347075?imp-id=2&charset=UTF-8&target-ref=http%3A%2F%2Flimehd.ru&page-ref=http%3A%2F%2Flimehd.ru&rnd={random}',
-        //'https://out.pladform.ru/getVast?pl=116720&type=preroll&license=1&thematic=1762&age=5&duration=0&target=mobile&adformat=3&dl=https://limehd.tv',
-        //'https://px.adhigh.net/p/direct_vast?pid=172&tid=instream_spbtv',
-        //'https://out.pladform.ru/getVast?pl=116722&type=preroll&license=1&thematic=1762&age=4&duration=0&target=mobile&adformat=3&dl=https://limehd.tv',
-        //'https://an.yandex.ru/meta/290606?imp-id=2&charset=UTF-8&target-ref=http%3A%2F%2Flimehd.ru&page-ref=http%3A%2F%2Flimehd.ru&rnd={random}',
-        //'https://data.videonow.ru/?profile_id=2458118&format=vast&container=preroll'
-    ];
-    
-    const getAdArray = () => {
-        adArr = [];
-        
-        for (let ad in adUrls) {
-            adUrl = adUrls[ad];
-            
-            adUrl = adUrl.replace('{random}', random(1, 4294967295), 'g');
-            adArr.push(adUrl);
-        }
-        
-        return adArr;
-    };
-    
-    const ads = getAdArray();
+$AD = $embed['ad_url'];
 
-const player = new OpenPlayer('video', ads, false, {
+$JW = <<<JS
+const random = (min, max) => {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+};
+
+let AD_URL = '$AD';
+AD_URL = AD_URL.replace('{random}', random(1, 4294967295), 'g');
+
+const player = new OpenPlayer('video', AD_URL, false, {
     hls: {
         startLevel: -1,
         enableWorker: true,
