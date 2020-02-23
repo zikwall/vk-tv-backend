df14a1ec8f151f0029ad6c943fd35d297d735a1d
zikwall <andrey.kapitonov.96@gmail.com>
2020-02-23 21:44:32 +0300

    Review

--- src/services/ContentService.php
+++ src/services/ContentService.php
@@ -23,7 +23,7 @@ class ContentService
         $content->own_player_url        = $contentAttributes['own_player_url'];
         $content->age_limit             = (int) $contentAttributes['is_18_years_old'] ? 50 : 10;
         $content->created_at            = time();
-        $content->updated_at            = 0;
+        $content->updated_at            = !!$content->id ? time() : 0;
         $content->is_auth_required      = 0;
         $content->visibility            = \zikwall\vktv\constants\Content::VISIBILITY_PUBLIC;
         $content->pinned                = (int) $contentAttributes['is_pinned'];
@@ -31,7 +31,8 @@ class ContentService
         $content->active                = (int) $contentAttributes['is_active'];
         $content->ad_url                = $contentAttributes['ad_url'];
         $content->use_origin            = $contentAttributes['use_origin'];
-        $content->blocked               = 0;
+        $content->default_player        = $contentAttributes['default_player'];
+        $content->blocked               = !!$content->id ? 0 : $content->blocked; //  Omg
 
         if (!$content->save()) {
             return false;
@@ -54,6 +55,7 @@ class ContentService
         $playlist->use_origin   = $content->use_origin ?? 0;
         $playlist->category     = $content->category;
         $playlist->ad_url       = $content->ad_url;
+        $playlist->default_player = $content->default_player;
 
         if (!$playlist->save()) {
             return false;
