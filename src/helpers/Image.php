c2193325c8ad0eeb497cdb9507dfd4c65b23c155
AndreyKapitonov <dj-on-ik@mail.ru>
2020-02-28 10:46:55 +0300

    Test base 64 save

--- src/helpers/Image.php
+++ src/helpers/Image.php
@@ -4,10 +4,17 @@ namespace zikwall\vktv\helpers;
 
 class Image
 {
-    public static function base64ToJPEG( $base64String, $outputFile ) {
-        $ifp = fopen( $outputFile, "wb" );
-        fwrite( $ifp, base64_decode( $base64String) );
-        fclose( $ifp );
-        return( $outputFile );
+    public static function base64ToJPEG( $base64String, $outputFile )
+    {
+        // split the string on commas
+        // $data[ 0 ] == "data:image/png;base64"
+        // $data[ 1 ] == <actual base64 string>
+        $data = explode( ',', $base64String );
+
+        $filenamePath = md5(time().uniqid()).".jpg";
+        //$decoded = base64_decode($base64String);
+        file_put_contents($outputFile, $data);
+
+        return $filenamePath;
     }
 }
