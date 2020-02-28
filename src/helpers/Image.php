e3863ef400fa02f8e78a381de9db94c5bb5523db
AndreyKapitonov <dj-on-ik@mail.ru>
2020-02-28 10:48:50 +0300

    Test base 64 save

--- src/helpers/Image.php
+++ src/helpers/Image.php
@@ -6,15 +6,20 @@ class Image
 {
     public static function base64ToJPEG( $base64String, $outputFile )
     {
+        // open the output file for writing
+        $ifp = fopen( $outputFile, 'wb' );
+
         // split the string on commas
         // $data[ 0 ] == "data:image/png;base64"
         // $data[ 1 ] == <actual base64 string>
         $data = explode( ',', $base64String );
 
-        $filenamePath = md5(time().uniqid()).".jpg";
-        //$decoded = base64_decode($base64String);
-        file_put_contents($outputFile, $data);
+        // we could add validation here with ensuring count( $data ) > 1
+        fwrite( $ifp, base64_decode( $data[ 1 ] ) );
 
-        return $filenamePath;
+        // clean up the file resource
+        fclose( $ifp );
+
+        return $outputFile;
     }
 }
